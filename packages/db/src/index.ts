8ab9396fa15e302b89b1a2f896cad9349697d09d
elijahnikov <enkv@proton.me>
2025-01-31 18:03:30 +0000

    .

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -8,18 +8,18 @@ export * from "@prisma/client";
 
 // Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
 const prismaClientSingleton = () => {
-  return new PrismaClient({
-    log:
-      process.env.NODE_ENV === "development"
-        ? ["query", "error", "warn"]
-        : ["error"],
-  });
+	return new PrismaClient({
+		log:
+			process.env.NODE_ENV === "development"
+				? ["query", "error", "warn"]
+				: ["error"],
+	});
 };
 
 type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
+	prisma: PrismaClientSingleton | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? prismaClientSingleton();
