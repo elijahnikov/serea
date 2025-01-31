1007eba5848ad42c2311c68813f1866d5d1cc0b8
elijahnikov <enkv@proton.me>
2025-01-31 21:58:51 +0000

    set up biome

--- packages/api/src/router/post.ts
+++ packages/api/src/router/post.ts
@@ -1,24 +1,25 @@
 import type { TRPCRouterRecord } from "@trpc/server";
 import { z } from "zod";
 
-
 import { protectedProcedure, publicProcedure } from "../trpc";
 
 export const postRouter = {
-  all: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany({ orderBy: { id: "desc" } });
-  }),
-  byId: publicProcedure
-    .input(z.object({ id: z.number() }))
-    .query(({ ctx, input }) => {
-      return ctx.db.post.findFirst({ where: { id: input.id } });
-    }),
-  create: protectedProcedure
-    .input(z.object({ name: z.string() }))
-    .mutation(({ ctx, input }) => {
-      return ctx.db.post.create({ data: { name: input.name, createdById: ctx.session.user.id } });
-    }),
-  delete: protectedProcedure.input(z.number()).mutation(({ ctx, input }) => {
-    return ctx.db.post.delete({ where: { id: input } });
-  }),
+	all: publicProcedure.query(({ ctx }) => {
+		return ctx.db.post.findMany({ orderBy: { id: "desc" } });
+	}),
+	byId: publicProcedure
+		.input(z.object({ id: z.number() }))
+		.query(({ ctx, input }) => {
+			return ctx.db.post.findFirst({ where: { id: input.id } });
+		}),
+	create: protectedProcedure
+		.input(z.object({ name: z.string() }))
+		.mutation(({ ctx, input }) => {
+			return ctx.db.post.create({
+				data: { name: input.name, createdById: ctx.session.user.id },
+			});
+		}),
+	delete: protectedProcedure.input(z.number()).mutation(({ ctx, input }) => {
+		return ctx.db.post.delete({ where: { id: input } });
+	}),
 } satisfies TRPCRouterRecord;
